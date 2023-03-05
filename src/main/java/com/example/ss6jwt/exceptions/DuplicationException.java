ecc6ba1772d2bf7a5c600a16bbfa18f578868b96
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 15:00:23 -0400

    Added DuplicationException and NotFoundException classes.

---
+++ src/main/java/com/example/ss6jwt/exceptions/DuplicationException.java
@@ -0,0 +1,10 @@
+package com.example.ss6jwt.exceptions;
+
+public class DuplicationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DuplicationException(String message) {
+		super(message);
+	}
+	
+}
