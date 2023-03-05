a7351ddc5308572057f84ef409556e601599382f
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 19:51:57 -0400

    Added @Service annotation on UserDetailsServiceImpl class.

--- src/main/java/com/example/ss6jwt/services/UserDetailsServiceImpl.java
+++ src/main/java/com/example/ss6jwt/services/UserDetailsServiceImpl.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
