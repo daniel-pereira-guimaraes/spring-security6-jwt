f64ac1115311ed29879ca9c8d4b8801cb2c8e5cd
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 11:35:00 -0400

    Initial project.

---
+++ src/main/java/com/example/ss6jwt/SpringSecurity6JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.ss6jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ss6jwt/SpringSecurity6JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss6jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
