4a9f08c63c9f8ed1472d6cfc678c5a474b958cb7
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 15:06:53 -0400

    Added UserDetailsServiceImpl class.

---
+++ src/main/java/com/example/ss6jwt/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.ss6jwt.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private PersonService personService;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		try {
+			return personService.findByEmail(username);
+		}
+		catch (Exception e) {
+			throw new UsernameNotFoundException(e.getMessage());
+		}
+	}
+
+}
