4f358177bc3971862cf685ee486337ef6caf3c6b
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 18:30:28 -0400

    Added AuthenticationManager bean on SecurityConfig class.

--- src/main/java/com/example/ss6jwt/config/SecurityConfig.java
+++ src/main/java/com/example/ss6jwt/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.ss6jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -35,6 +37,11 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 	
 	@Bean
