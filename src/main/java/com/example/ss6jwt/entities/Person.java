4cec67122ec7ae2cbb9bfea87bec134769ccb047
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 13:53:55 -0400

    Added UserDetails implementation on Person class.

--- src/main/java/com/example/ss6jwt/entities/Person.java
+++ src/main/java/com/example/ss6jwt/entities/Person.java
@@ -1,9 +1,14 @@
 package com.example.ss6jwt.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.ss6jwt.enums.Role;
 
@@ -18,7 +23,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 
 @Entity
-public class Person implements Serializable {
+public class Person implements Serializable, UserDetails {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -91,6 +96,38 @@ public class Person implements Serializable {
 
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles.stream().map(r -> r.getId()).collect(Collectors.toSet());
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+				.map(r -> new SimpleGrantedAuthority(Role.fromId(r).name()))
+				.collect(Collectors.toSet());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 }
