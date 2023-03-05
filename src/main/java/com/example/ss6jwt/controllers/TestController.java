bec96542c73813b566778fab1685874228558594
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 16:53:50 -0400

    Added endpoint on TestController class to test token generation.

--- src/main/java/com/example/ss6jwt/controllers/TestController.java
+++ src/main/java/com/example/ss6jwt/controllers/TestController.java
@@ -1,13 +1,19 @@
 package com.example.ss6jwt.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ss6jwt.services.JwtService;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
+	
+	@Autowired
+	private JwtService jwtService;
 
 	@GetMapping("/ok")
 	public ResponseEntity<String> ok() {
@@ -17,5 +23,10 @@ public class TestController {
 	@GetMapping("/error")
 	public ResponseEntity<String> error() {
 		throw new RuntimeException("Test error!");
+	}
+	
+	@GetMapping("/gen/token")
+	public ResponseEntity<String> genToken() {
+		return ResponseEntity.ok(jwtService.generateToken("test@mail.com"));
 	}
 }
