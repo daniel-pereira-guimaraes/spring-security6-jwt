c232c57e20804875d1fad742e441f846f00cebfc
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 20:47:41 -0400

    Added jwtAuthFilter on SecurityConfig.

--- src/main/java/com/example/ss6jwt/config/SecurityConfig.java
+++ src/main/java/com/example/ss6jwt/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -28,6 +29,9 @@ public class SecurityConfig {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtAuthFilter jwtAuthFilter;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -39,6 +43,7 @@ public class SecurityConfig {
 				.requestMatchers(PUBLIC).permitAll()
 				.anyRequest().authenticated()
 				.and()
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
 	
