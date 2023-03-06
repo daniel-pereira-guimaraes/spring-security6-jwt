292626018877cf6cead22b725848532b7bc0e155
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-05 22:28:31 -0400

    Added PersonDTO class.

---
+++ src/main/java/com/example/ss6jwt/dtos/PersonDTO.java
@@ -0,0 +1,69 @@
+package com.example.ss6jwt.dtos;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.example.ss6jwt.entities.Person;
+import com.example.ss6jwt.enums.Role;
+
+public class PersonDTO {
+	
+	private Long id;
+	private String name;
+	private String email;
+	private Set<String> roles = new HashSet<>();
+	
+	public PersonDTO() {
+		super();
+	}
+
+	public PersonDTO(Long id, String name, String email, Set<String> roles) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.roles = roles;
+	}
+	
+	public PersonDTO(Person person) {
+		super();
+		this.id = person.getId();
+		this.name = person.getName();
+		this.email = person.getEmail();
+		this.setRoles(person.getRoles());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Set<String> getRoles() {
+		return roles;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles.stream().map(r -> r.getDescription()).collect(Collectors.toSet());
+	}
+	
+}
