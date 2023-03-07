03507d213a8b82fee8efe7c5aff04dadeaeac326
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2023-03-06 20:15:12 -0400

    Added create, update and delete methods on PersonController class.

--- src/main/java/com/example/ss6jwt/controllers/PersonController.java
+++ src/main/java/com/example/ss6jwt/controllers/PersonController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +23,30 @@ public class PersonController {
 	@Autowired
 	private PersonService service;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@GetMapping
 	public ResponseEntity<List<PersonDTO>> findAll() {
 		final List<Person> persons = service.findAll();
 		final List<PersonDTO> dtos = persons.stream().map(p -> new PersonDTO(p)).toList();
 		return ResponseEntity.ok(dtos);
+	}
+	
+	public ResponseEntity<PersonDTO> create(@RequestBody PersonDTO dto) {
+		dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+		return ResponseEntity.ok(service.create(dto));
+	}
+	
+	public ResponseEntity<PersonDTO> update(@RequestBody PersonDTO dto) {
+		dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+		return ResponseEntity.ok(service.create(dto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
